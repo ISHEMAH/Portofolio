8f777f8c49dfbae6077b98db21103db7f6c58c8e
ISHEMAH <121429065+ISHEMAH@users.noreply.github.com>
2023-01-23 17:53:33 -0800

    first commit

---
+++ myweb.js
@@ -0,0 +1,34 @@
+// let options = document.querySelectorAll('.passive_a');
+
+//    options[0].addEventListener('click',function(){
+//     for (let i = 0;i<options.length;i++){
+//     options[i].classList.remove("active_a");
+//     options[0].classList.add("active_a");
+//     options[0].classList.remove("passive_a");
+//     options[i].classList.add("passive_a");
+// }
+//    })
+//    options[1].addEventListener('click',function(){
+//     for (let i = 0;i<options.length;i++){
+//     options[i].classList.remove("active_a");
+//     options[1].classList.add("active_a");
+//     options[1].classList.remove("passive_a");
+//     options[i].classList.add("passive_a");
+// }
+//    })
+//    options[2].addEventListener('click',function(){
+//     for (let i = 0;i<options.length;i++){
+//     options[i].classList.remove("active_a");
+//     options[2].classList.add("active_a");
+//     options[2].classList.remove("passive_a");
+//     options[i].classList.add("passive_a");
+// }
+//    })
+//    options[3].addEventListener('click',function(){
+//     for (let i = 0;i<options.length;i++){
+//     options[i].classList.remove("active_a");
+//     options[3].classList.add("active_a");
+//     options[3].classList.remove("passive_a");
+//     options[i].classList.add("passive_a");
+// }
+//    })
